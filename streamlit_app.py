eb1d230ee1f82b29c3dc5f104378e0b8189ef661
xyverrt <42400431+xyverrt@users.noreply.github.com>
2023-05-15 19:42:14 +0800

    Update streamlit_app.py
    fix typo for requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,5 +22,5 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 import requests
-fruityvice_response = request.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
