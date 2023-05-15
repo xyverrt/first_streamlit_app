abf4eb073972578ad34306645170ecb5dacef038
xyverrt <42400431+xyverrt@users.noreply.github.com>
2023-05-15 22:30:09 +0800

    Update streamlit_app.py
    fix syntax error

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,8 +35,8 @@ streamlit.header('View Our Fruit List - Add Your Favorites!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
-    streamlit.error("Please select a fruit to get information.")
-    else:
+      streamlit.error("Please select a fruit to get information.")
+  else:
       back_from_function =  get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
       
