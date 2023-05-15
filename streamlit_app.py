446cb40aeeac8677eb91012db857c4cd962697ec
xyverrt <42400431+xyverrt@users.noreply.github.com>
2023-05-15 19:48:10 +0800

    Update streamlit_app.py
    fix typo and remove raw json response

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,10 +24,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 
 #take the json version of the response and normalize it
-fruityvice_normalized = pandas.jason_normalize(fruityvice_response.json())
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output it on the screen as a table
 streamlit.dataframe(fruityvice_normalized)
