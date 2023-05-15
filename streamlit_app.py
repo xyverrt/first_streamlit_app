d9b950f79eaa40e882be88b99552d938cb3d1c6f
xyverrt <42400431+xyverrt@users.noreply.github.com>
2023-05-15 22:31:41 +0800

    Update streamlit_app.py
    fix syntax error URLError

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ try:
       back_from_function =  get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
       
- except URLError as e:
+except URLError as e:
   streamlit.error()
   
 streamlit.stop()
